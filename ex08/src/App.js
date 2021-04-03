ee5eaeab086ba5162966b89c45867dcfe3d99464
Tarik2021 <tariks2021@gmail.com>
2021-04-03 08:09:31 +0200

    Last day of Arena

---
+++ ex08/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PostRequestSetHeader from './PostRequestSetHeader';
+
+function App() {
+  return (
+    <div className="App">
+      <PostRequestSetHeader />
+    </div>
+  );
+}
+
+export default App;
